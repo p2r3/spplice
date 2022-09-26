d5da20759beb06a4fcc0ead33bee29a494b72dd0
p2r3 <p2r3@p2r3.com>
2022-09-26 23:03:51 +0300

    Use wmic instead of taskkill

--- resources/js/modloader.js
+++ resources/js/modloader.js
@@ -43,7 +43,7 @@ async function getGameDirectory(steamPath) {
 async function killGame() {
 
   switch (NL_OS) {
-    case "Windows": await Neutralino.os.execCommand('start /b taskkill /fi "IMAGENAME eq portal2.exe" /f');
+    case "Windows": await Neutralino.os.execCommand('wmic process where name="portal2.exe" delete');
     case "Linux"  : await Neutralino.os.execCommand('pkill -9 "portal2_linux"');
     case "Darwin" : await Neutralino.os.execCommand('pkill -9 "portal2_osx"');
   }
